ea6a4e22ed63bf9e22f70e54682af16a3553390a
Joshua Ham <Joshuaju.ham@gmail.com>
2023-02-01 23:08:33 -0800

    added call to match  history

--- check-your-league/src/pages/page/Page.jsx
+++ check-your-league/src/pages/page/Page.jsx
@@ -11,14 +11,17 @@ function Page() {
   useEffect(() => {
     axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}`, {params: {"api_key": process.env.REACT_APP_RIOT_API}})
       .then((results) => {
-        console.log(results.data);
+        axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${results.data.puuid}/ids`, {params: {"api_key": process.env.REACT_APP_RIOT_API}})
+          .then((matches) => {
+            console.log(matches.data);
+          })
       })
   }, [])
 
   return (
-    <>
+    <div className="pageContainer">
       <div>This is {name}</div>
-    </>
+    </div>
   )
 }
 
