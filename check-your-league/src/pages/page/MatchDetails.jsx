23e429e9ae843b87c2bd760ccfacc165a67890bc
Joshua Ham <Joshuaju.ham@gmail.com>
2023-02-04 03:22:46 -0800

    added separate component for matches

---
+++ check-your-league/src/pages/page/MatchDetails.jsx
@@ -0,0 +1,14 @@
+import './matchDetails.css';
+
+function MatchDetails({matchData}) {
+  console.log(matchData);
+
+  return (
+    <>
+      <div>hello</div>
+    </>
+  )
+}
+
+
+export default MatchDetails;
