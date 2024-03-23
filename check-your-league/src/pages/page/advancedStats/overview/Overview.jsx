c5caf38df404d303949abdda344c7d6b5ffd0144
Joshua Ham <Joshuaju.ham@gmail.com>
2024-03-23 05:09:05 -0700

    cleaned up matchDetails

---
+++ check-your-league/src/pages/page/advancedStats/overview/Overview.jsx
@@ -0,0 +1,18 @@
+import {useTable} from 'react-table';
+
+function Overview({matchData}) {
+  // console.log(matchData);
+
+  return (
+    <div className="overviewContainer">
+      {/* {matchData.info.participants.map((value, index) =>
+        <div className="playerValues">
+
+        </div>
+      )} */}
+    </div>
+  )
+};
+
+export default Overview;
+
