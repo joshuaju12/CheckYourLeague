25971435de73c82256137815f2e71aca16d7091e
Joshua Ham <Joshuaju.ham@gmail.com>
2024-03-26 16:05:19 -0700

    added basic table design and layout for overview

---
+++ check-your-league/src/pages/page/advancedStats/overview/Table.jsx
@@ -0,0 +1,41 @@
+
+function Table({matchData, team}) {
+  let start;
+  const rows = [];
+  console.log(matchData);
+
+  if (team === 1) {
+    start = 0;
+    for (let i = 0; i < 5; i++) {
+      let playerData = matchData.info.participants[i];
+      rows.push(
+        <tr key={i}>
+          <td>{playerData.riotIdGameName}</td>
+          <td>{playerData.kills}/{playerData.deaths}/{playerData.assists}</td>
+        </tr>
+      )
+    }
+    console.log(rows);
+  } else {
+    start = 5;
+  }
+
+
+
+  console.log(start);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>KDA</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  )
+};
+
+export default Table;
