a284f117f3a1b96535dda34ee6eb68d0346d5c4d
Joshua Ham <Joshuaju.ham@gmail.com>
2024-05-06 05:25:20 -0700

    added logic for getting data in TeamGraph.jsx

--- check-your-league/src/pages/page/tabs/stats/Stats.jsx
+++ check-your-league/src/pages/page/tabs/stats/Stats.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect, useMemo} from 'react';
+import {useState, useEffect} from 'react';
 import axios from 'axios';
 import Select from 'react-select';
 import './stats.css';
