1146d5b6c2b26f5d938f25ad7748805b24e8aace
Joshua Ham <Joshuaju.ham@gmail.com>
2024-05-16 05:32:19 -0700

    started on teamDamage.jsx

--- check-your-league/src/pages/page/tabs/teamGraph/TeamDamage.jsx
+++ check-your-league/src/pages/page/tabs/teamGraph/TeamDamage.jsx
@@ -2,24 +2,62 @@ import './teamDamage.css';
 
 function TeamDamage ({data, matchData}) {
   console.log(matchData);
-  const playerData = {};
-  const getHighestDamage = () => {
+  const teamOnePlayerData = [];
+  const teamTwoPlayerData = [];
+  let highestDamage;
+
+  const getDamage = () => {
     const allPlayers = matchData.info.participants
     for (let i = 0; i < allPlayers.length; i++) {
-      if (!playerData.highest) {
-        playerData.highest = {name:allPlayers[i].championName, value: allPlayers[i].totalDamageDealtToChampions};
+      if (!highestDamage) {
+        highestDamage = allPlayers[i].totalDamageDealtToChampions;
       } else {
-        if (allPlayers[i].totalDamageDealtToChampions > playerData.highest.value) {
-          playerData.highest = {name: allPlayers[i].championName, value: allPlayers[i].totalDamageDealtToChampions};
+        if (allPlayers[i].totalDamageDealtToChampions > highestDamage) {
+          highestDamage = allPlayers[i].totalDamageDealtToChampions;
         }
       }
-      // playerData[i] = {championName:}
+      if (i < 5) {
+        teamOnePlayerData.push({championName: allPlayers[i].championName, playerName: allPlayers[i].riotIdGameName, damage: allPlayers[i].totalDamageDealtToChampions});
+      } else {
+        teamTwoPlayerData.push({championName: allPlayers[i].championName, playerName: allPlayers[i].riotIdGameName, damage: allPlayers[i].totalDamageDealtToChampions});
+      }
     }
-  }
+  };
+
+  getDamage();
 
   return(
-    <div>
-      this should show
+    <div className="teamDamageContainer">
+      <div className="teamDamageTeamOne">
+        {teamOnePlayerData.map((value, index) => (
+          teamOnePlayerData[index].damage < highestDamage
+            ? (<div key={index}>
+                 <img className="teamDamageImage" src={require(`../overview/assets/champions/${teamOnePlayerData[index].championName}.png`)} alt="" />
+                 <div className="teamDamageStatsContainer">
+                   <div className="teamDamagePlayerName">{teamOnePlayerData[index].playerName}</div>
+                   <div className="teamDamageDamageValue">{teamOnePlayerData[index].damage}</div>
+                   <div className="teamDamageDamageBar">bar</div>
+                 </div>
+               </div>
+              )
+            : (<div key={index}>
+                 <img className="teamDamageImage" src={require(`../overview/assets/champions/${teamOnePlayerData[index].championName}.png`)} alt="" />
+                 <div className="teamDamageStatsContainer">
+                   <div className="teamDamagePlayerName">{teamOnePlayerData[index].playerName}</div>
+                   <div className="teamDamageDamageValue">{teamOnePlayerData[index].damage}</div>
+                   <div className="teamDamageDamageBar">highestDamageBar</div>
+                 </div>
+               </div>
+              )
+        ))}
+      </div>
+      <div className="teamDamageTeamTwo">
+        {/* {teamTwoPlayerData.map((value, index) => (
+          teamTwoPlayerData[index].damage < highestDamage
+            ? (<div key={index}>{teamTwoPlayerData[index].damage}</div>)
+            : (<div key={index}>{teamTwoPlayerData[index].damage}</div>)
+        ))} */}
+      </div>
     </div>
   )
 }
