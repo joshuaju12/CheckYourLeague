bf470329ce457570e21c455e102ea406eedb3e4f
Joshua Ham <Joshuaju.ham@gmail.com>
2024-05-16 00:27:37 -0700

    added some new item assets

--- check-your-league/src/pages/page/tabs/teamGraph/TeamDamage.jsx
+++ check-your-league/src/pages/page/tabs/teamGraph/TeamDamage.jsx
@@ -1,6 +1,22 @@
+import './teamDamage.css';
 
+function TeamDamage ({data, matchData}) {
+  console.log(matchData);
+  const playerData = {};
+  const getHighestDamage = () => {
+    const allPlayers = matchData.info.participants
+    for (let i = 0; i < allPlayers.length; i++) {
+      if (!playerData.highest) {
+        playerData.highest = {name:allPlayers[i].championName, value: allPlayers[i].totalDamageDealtToChampions};
+      } else {
+        if (allPlayers[i].totalDamageDealtToChampions > playerData.highest.value) {
+          playerData.highest = {name: allPlayers[i].championName, value: allPlayers[i].totalDamageDealtToChampions};
+        }
+      }
+      // playerData[i] = {championName:}
+    }
+  }
 
-function TeamDamage ({data}) {
   return(
     <div>
       this should show
