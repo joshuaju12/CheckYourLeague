0137bd0102370bdfbcddb15597e26eb39c0cbd9d
Joshua Ham <Joshuaju.ham@gmail.com>
2024-11-01 19:43:46 -0700

    added rank assets and finished rank statistics

--- check-your-league/src/pages/page/PlayerStatistics.jsx
+++ check-your-league/src/pages/page/PlayerStatistics.jsx
@@ -2,14 +2,18 @@ import './playerStatistics.css';
 
 function PlayerStatistics({rankedInfo}) {
 
-  // console.log(rankedInfo);
-
   let lowestRank = "Unranked";
   let lowestTier = "";
   let lowestType = "";
+  let lowestLP = 0;
+  let lowestWin = 0;
+  let lowestLoss = 0;
   let highestRank = "Unranked";
   let highestTier = "";
   let highestType = "";
+  let highestLP = 0;
+  let highestWin = 0;
+  let highestLoss = 0;
 
   const rankedValues = {
     "IRON" : 0,
@@ -25,8 +29,8 @@ function PlayerStatistics({rankedInfo}) {
   };
 
   const rankedTypes = {
-    "RANKED_SOLO_5X5" : "Solo/Duo",
-    "RANKED_FLEX_SR" : "Flex",
+    "RANKED_SOLO_5x5" : "Ranked Solo/Duo",
+    "RANKED_FLEX_SR" : "Ranked Flex",
   };
 
   const rankedTier = {
@@ -36,45 +40,137 @@ function PlayerStatistics({rankedInfo}) {
     "IV" : '4',
   };
 
+
+
   if (rankedInfo.length > 1) {
     let rankOne = rankedValues[rankedInfo[0].tier] + rankedInfo[0].leaguePoints;
     let rankTwo = rankedValues[rankedInfo[1].tier] + rankedInfo[1].leaguePoints;
 
     if (rankOne > rankTwo) {
-
       highestRank = rankedInfo[0].tier;
       highestRank = highestRank[0].toUpperCase() + highestRank.slice(1).toLowerCase();
       highestTier = rankedTier[rankedInfo[0].rank];
       highestType = rankedTypes[rankedInfo[0].queueType];
+      highestLP = rankedInfo[0].leaguePoints;
+      highestWin = rankedInfo[0].wins;
+      highestLoss = rankedInfo[0].losses;
       lowestRank = rankedInfo[1].tier;
-      lowestRank = lowestRank[0].toUpperCase() + highestRank.slice(1).toLowerCase();
+      lowestRank = lowestRank[0].toUpperCase() + lowestRank.slice(1).toLowerCase();
       lowestTier = rankedTier[rankedInfo[1].rank];
       lowestType = rankedTypes[rankedInfo[1].queueType];
+      lowestLP = rankedInfo[1].leaguePoints;
+      lowestWin = rankedInfo[1].wins;
+      lowestLoss = rankedInfo[1].losses;
     } else {
       highestRank = rankedInfo[1].tier;
       highestRank = highestRank[0].toUpperCase() + highestRank.slice(1).toLowerCase();
       highestTier = rankedTier[rankedInfo[1].rank];
       highestType = rankedTypes[rankedInfo[1].queueType];
+      highestLP = rankedInfo[1].leaguePoints;
+      highestWin = rankedInfo[1].wins;
+      highestLoss = rankedInfo[1].losses;
       lowestRank = rankedInfo[0].tier;
-      lowestRank = lowestRank[0].toUpperCase() + highestRank.slice(1).toLowerCase();
+      lowestRank = lowestRank[0].toUpperCase() + lowestRank.slice(1).toLowerCase();
       lowestTier = rankedTier[rankedInfo[0].rank];
       lowestType = rankedTypes[rankedInfo[0].queueType];
+      lowestLP = rankedInfo[0].leaguePoints;
+      lowestWin = rankedInfo[0].wins;
+      lowestLoss = rankedInfo[0].losses;
     };
   } else {
-    highestRank = rankedValues[rankedInfo[0].tier];
-    highestTier = rankedTier[rankedInfo[0].rank];
-    highestType = rankedTypes[rankedInfo[0].queueType];
-    if (highestType === "Solo/Duo") {
-      lowestType = "Flex";
-    } else  {
-      lowestType = "Solo/Duo";
+    if (rankedInfo.length === 1) {
+      highestRank = rankedInfo[0].tier;
+      highestRank = highestRank[0].toUpperCase() + highestRank.slice(1).toLowerCase();
+      highestTier = rankedTier[rankedInfo[0].rank];
+      highestType = rankedTypes[rankedInfo[0].queueType];
+      highestLP = rankedInfo[0].leaguePoints;
+      highestWin = rankedInfo[0].wins;
+      highestLoss = rankedInfo[0].losses;
+      if (highestType === "Solo/Duo") {
+        lowestType = "Ranked Flex";
+      } else  {
+        lowestType = "Ranked Solo/Duo";
+      }
+    }  else {
+      highestType = "Ranked Solo/Duo";
+      lowestType = "Ranked Flex";
     }
   }
 
   return (
     <div className="playerStatisticsContainer">
-      <div className="highestRankContainer"></div>
-      <div className="lowestRankContainer"></div>
+      <div className="rankContainerWrapper">
+        {rankedInfo.length === 0 ?
+          <div className ="rankContainer">
+            <div className="rankTypeWrapper">
+              <div className="rankType">
+                {highestType}
+              </div>
+            </div>
+            <div className="rankInfo">
+              <div className="rankInfoPlaceholder"></div>
+            </div>
+            <div>Unranked</div>
+          </div> :
+          <div className="rankContainer">
+            <div className="rankTypeWrapper">
+              <div className="rankType">
+                {highestType}
+              </div>
+            </div>
+            <div className="rankInfo">
+              <img className="rankImage" src={require(`./assets/ranks/${highestRank.toUpperCase()}.png`)} alt="" />
+            </div>
+            <div>{highestRank} {highestTier}</div>
+            <div className="rankStatisticsContainer">
+              <div>{highestWin}W {highestLoss}L</div>
+              <div>{highestLP} LP</div>
+              <div>Win Rate {Math.floor(highestWin / (highestWin + highestLoss) * 100)}%</div>
+            </div>
+          </div>
+        }
+      </div>
+      <div className="rankContainerWrapper">
+        {rankedInfo.length === 0 ?
+          <div className ="rankContainer">
+          <div className="rankTypeWrapper">
+            <div className="rankType">
+              {lowestType}
+            </div>
+          </div>
+          <div className="rankInfo">
+            <div className="rankInfoPlaceholder"></div>
+          </div>
+          <div>Unranked</div>
+        </div> :
+        <div className="rankContainer">
+          <div className="rankTypeWrapper">
+            <div className="rankType">
+              {lowestType}
+            </div>
+          </div>
+          {rankedInfo.length === 1 ?
+            <>
+              <div className="rankInfo">
+                <div className="rankInfoPlaceholder"></div>
+              </div>
+              <div>Unranked</div>
+            </> :
+            <>
+            <div className="rankInfo">
+                <img className="rankImage" src={require(`./assets/ranks/${lowestRank.toUpperCase()}.png`)} alt="" />
+              </div>
+              <div>{lowestRank} {lowestTier}</div>
+              <div className="rankStatisticsContainer">
+                <div>{lowestWin}W {lowestLoss}L</div>
+                <div>{lowestLP} LP</div>
+                <div>Win Rate {Math.floor(lowestWin / (lowestWin + lowestLoss) * 100)}%</div>
+              </div>
+            </>
+          }
+        </div>
+        }
+      </div>
     </div>
   )
 }
