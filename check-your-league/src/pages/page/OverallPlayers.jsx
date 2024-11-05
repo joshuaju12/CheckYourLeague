674e68779a9978fd70d6878fa16507441858f402
Joshua Ham <Joshuaju.ham@gmail.com>
2024-11-05 02:12:23 -0800

    added loading component for when another player is selected in dropdown in stats tab

--- check-your-league/src/pages/page/OverallPlayers.jsx
+++ check-your-league/src/pages/page/OverallPlayers.jsx
@@ -4,20 +4,22 @@ function OverallPlayers ({players}) {
 
   let rows0 = [];
   let rows1 = [];
+  let teamOneWin;
+  let teamTwoWin;
 
   for (let i = 0; i < players.length; i++) {
     if (i <= 4) {
       rows0.push(
         <div className="playersRowOne" key={i}>
           <img src={require(`./tabs/overview/assets/champions/${players[i].championName}.png`)} alt='' />
-          <div>{players[i].riotIdGameName}</div>
+          <div className="overallPlayersName">{players[i].riotIdGameName}</div>
         </div>
       )
     } else {
       rows1.push(
         <div className="playersRowTwo" key={i}>
           <img src={require(`./tabs/overview/assets/champions/${players[i].championName}.png`)} alt='' />
-          <div>{players[i].riotIdGameName}</div>
+          <div className="overallPlayersName">{players[i].riotIdGameName}</div>
         </div>
       )
     }
